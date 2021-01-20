c0500e5ab5e9ba8d4e55889f7f9f48300f02f0ba
mtkmr <m.takamura0520@gmail.com>
2021-01-20 16:35:48 +0900

    Initial Commit

---
+++ TableView_Practice01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView_Practice01
+//
+//  Created by Masato Takamura on 2021/01/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
