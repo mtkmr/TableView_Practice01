d87319db9d0c0d16d27b312758b575b37328be9f
mtkmr <m.takamura0520@gmail.com>
2021-01-20 16:48:20 +0900

    first commit.

--- TableView_Practice01/ViewController.swift
+++ TableView_Practice01/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    private let items = ["A", "B", "C", "D", "1", "2", "3", "4"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        
     }
 
 
+}
+
+//tableViewにデータを流し込む
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") ?? UITableViewCell(style: .default, reuseIdentifier: "cell")
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
+    
+    
 }
 
